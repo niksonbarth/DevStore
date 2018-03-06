257e0a4255d27cb6fe4791513b78882595e59c9e
niksonbarth <nikson.barth@benner.com.br>
2018-03-06 13:18:31 -0300

    Adicionando no Github

---
+++ DevStore.Api/Controllers/ProductsController.cs
@@ -0,0 +1,229 @@
+﻿using DevStore.Domain;
+using DevStore.Infra.DataContext;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DevStore.Api.Controllers
+{
+    [EnableCors(origins: "http://localhost:64423", headers: "*", methods: "*")]
+    [RoutePrefix("api/v1/public")]
+    public class ProductController : ApiController
+    {
+        private DevStoreDataContext db = new DevStoreDataContext();
+
+        #region Products
+        [Route("products")]
+        public HttpResponseMessage GetProducts()
+        {
+            var result = db.Products.Include("Category").ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("products/{id}")]
+        public HttpResponseMessage GetProducts(int id)
+        {
+            if (id <= 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            var result = db.Products.Include("Category").Where(x => x.Id == id).FirstOrDefault();
+
+            if (result != null)
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "Não foi possível recuperar o produto");
+        }
+
+        [HttpPost]
+        [Route("products")]
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir produto");
+            }
+        }
+
+        [HttpPatch]
+        [Route("products")]
+        public HttpResponseMessage PatchProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar produto");
+            }
+        }
+
+        [HttpPut]
+        [Route("products")]
+        public HttpResponseMessage PutProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar produto");
+            }
+        }
+
+        [HttpDelete]
+        [Route("products")]
+        public HttpResponseMessage DeleteProduct(int productId)
+        {
+            if (productId <= 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Remove(db.Products.Find(productId));
+                db.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Produto excluido");
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao excluir produto");
+            }
+        }
+        #endregion
+
+        #region Categories
+        [Route("categories")]
+        public HttpResponseMessage GetCategories()
+        {
+            var result = db.Categories.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("categories/{categoryId}/products")]
+        public HttpResponseMessage GetProductsByCategory(int categoryId)
+        {
+            var result = db.Products.Include("Category").Where(x => x.CategoryId == categoryId).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [HttpPost]
+        [Route("categories")]
+        public HttpResponseMessage PostCategory(Category category)
+        {
+            if (category == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+
+                var result = category;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir categoria");
+            }
+        }
+
+        [HttpPatch]
+        [Route("categories")]
+        public HttpResponseMessage PatchCategory(Category category)
+        {
+            if (category == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Category>(category).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = category;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar categoria");
+            }
+        }
+
+        [HttpPut]
+        [Route("categories")]
+        public HttpResponseMessage PutCategory(Category category)
+        {
+            if (category == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Entry<Category>(category).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                var result = category;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar categoria");
+            }
+        }
+
+        [HttpDelete]
+        [Route("categories")]
+        public HttpResponseMessage DeleteCategory(int categoryId)
+        {
+            if (categoryId <= 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Categories.Remove(db.Categories.Find(categoryId));
+                db.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Categoria excluida");
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao excluir categoria");
+            }
+        }
+        #endregion
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DevStore.Infra/Mappings/ProductMap.cs
@@ -0,0 +1,26 @@
+﻿using DevStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStore.Infra.Mappings
+{
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable("Product");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Title).HasMaxLength(160).IsRequired();
+            Property(x => x.Price).IsRequired();
+            Property(x => x.AcquireDate).IsRequired();
+
+            HasRequired(x => x.Category);
+        }
+    }
+}
